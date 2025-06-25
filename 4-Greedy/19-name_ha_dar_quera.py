bdb823f0e9d406e50af454afd5434a73e4436648
MMD <mmd.gh313@gmail.com>
2025-06-26 02:55:38 +0330

    clean code

--- 4-Greedy/19-name_ha_dar_quera.py
+++ 4-Greedy/19-name_ha_dar_quera.py
@@ -2,14 +2,14 @@
 # https://quera.org/college/21026/chapter/81524/lesson/279303/?comments_page=1&comments_filter=ALL
 
 n, m = map(int, input().split())
-
 l = list(map(int, input().split()))
 
-data = [0] * m
+company = [0] * m
 max_rep = 0
+
 for i in l:
-    data[i-1] += 1
-    max_rep = max(max_rep, data[i-1])
+    company[i-1] += 1
+    max_rep = max(max_rep, company[i-1])
 
 # if n - max_rep < max_rep: NO
 print("NO" if n / 2 < max_rep else "YES")
